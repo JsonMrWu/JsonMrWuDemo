e791e718dd91882927269fe0024af40bcf6f8888
JsonMrWu <676089886@qq.com>
2016-07-07 17:36:17 +0800

    change view backgroundColor

--- demp/demp/ViewController.m
+++ demp/demp/ViewController.m
@@ -16,7 +16,9 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    // Do any addition al setup after loading the view, typically from a nib.
+    self.view.backgroundColor = [UIColor orangeColor];
+    
 }
 
 - (void)didReceiveMemoryWarning {
